40f94cce7ccee8d881f07c448b4649b599d63960
Antonin Arquey <arquey.antonin@gmail.com>
2017-12-12 17:56:57 +0100

    Cleaning repository for fresh start

--- src/js/webcam.js
+++ src/js/webcam.js
@@ -1,19 +1,45 @@
-(function () {
-    navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+// Older browsers might not implement mediaDevices at all, so we set an empty object first
+if (navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {};
+}
 
+// Some browsers partially implement mediaDevices. We can't just assign an object
+// with getUserMedia as it would overwrite existing properties.
+// Here, we will just add the getUserMedia property if it's missing.
+if (navigator.mediaDevices.getUserMedia === undefined) {
+  navigator.mediaDevices.getUserMedia = function getUM(constraints) {
+    // First get ahold of the legacy getUserMedia, if present
+    const getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-    navigator.getMedia(
-        // constraints
-        {video:true, audio:false},
+    // Some browsers just don't implement it - return a rejected promise with an error
+    // to keep a consistent interface
+    if (!getUserMedia) {
+      return Promise.reject(new Error('getUserMedia is not implemented in this browser'));
+    }
 
-        // success callback
-        function (mediaStream) {
-            var video = document.getElementsByTagName('video')[0];
-            video.src = window.URL.createObjectURL(mediaStream);
-            video.play();
-        },
-        //handle error
-        function (error) {
-            console.log(error);
-        })
-})();
+    // Otherwise, wrap the call to the old navigator.getUserMedia with a Promise
+    return new Promise((resolve, reject) => {
+      getUserMedia.call(navigator, constraints, resolve, reject);
+    });
+  };
+}
+// Prefer camera resolution nearest to 1280x720.
+const constraints = { audio: true, video: { width: 1280, height: 720 } };
+
+navigator.mediaDevices.getUserMedia(constraints)
+  .then((stream) => {
+    const video = document.querySelector('video');
+    // Older browsers may not have srcObject
+    if ('srcObject' in video) {
+      video.srcObject = stream;
+    } else {
+      // Avoid using this in new browsers, as it is going away.
+      video.src = window.URL.createObjectURL(stream);
+    }
+    video.onloadedmetadata = () => {
+      video.play();
+    };
+  })
+  .catch((err) => {
+    console.log(`${err.name} : ${err.message}`);
+  });

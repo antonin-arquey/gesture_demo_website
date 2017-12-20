5fac6caf7f459860ad8fd466ac21204153ba2ad7
Andréas PASTOR <andreas.pastor@etu.univ-nantes.fr>
2017-12-20 13:17:14 +0100

    bug correction in model prediction

--- src/js/conv.js
+++ src/js/conv.js
@@ -7,11 +7,14 @@ import model from '../json/model.json';
 const webcamManager = new WebcamManager(64, 64);
 const net = new convnetjs.Net(); // create an empty network
 net.fromJSON(model);
+//net.makeLayers(layer_defs);
+
+let trainer = new convnetjs.SGDTrainer(net, {method:'adadelta', batch_size:4, l2_decay:0.0001});
 
 export default function conv() {
   const data = webcamManager.takeSnapshot();
-  let arr = [[]];
-  let x = 0;
+  let arr = [];
+  let x = -1;
   for (let i = 0; i < data.length; i += 4) {
     if (i % 256 === 0) {
       arr.push([]);
@@ -19,17 +22,19 @@ export default function conv() {
     }
     arr[x].push(Math.round((data[i] + data[i + 1] + data[i + 2]) / 3));
   }
-  arr = arr.slice(1);
+  console.log('new value');
   console.log(arr);
 
-  const v = new convnetjs.Vol(64, 64, 1, 0);
+  const v = new convnetjs.Vol(64, 64, 1);
 
-  for (x = 0; x <= arr.length; x += 1) {
-    for (let y = 0; y <= arr[x].length; y += 1) {
-      v.set(x, y, 0, data[x][y]);
+  for (x = 0; x < arr.length; x += 1) {
+    for (let y = 0; y < arr[x].length; y += 1) {
+      v.set(y,x,0, arr[x][y]);
     }
   }
+
+  console.log(v);
   const probabilityVolume = net.forward(v);
-  console.log(probabilityVolume.w);
+  console.log(probabilityVolume);
   return probabilityVolume.w;
 }
